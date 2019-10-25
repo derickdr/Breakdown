9b796df214838c2cba13687db3d42eff06519c76
Derick Del Rosario <derickmitchel@icloud.com>
2019-10-25 10:27:55 -0700

    sign in page created, preliminary styles, navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,31 @@
 class User < ApplicationRecord
     
     has_secure_password
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new user_params
+        if @user.save
+            flash[:notice] = 'Success'
+        else
+            redirect_to user_new_path
+            flash[:notice] = 'Failure'
+        end
+    end
+
+    def show
+    end
+
+    def index
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'home/index'
+
+  get('/', {to: 'home#index', as: 'root'})
+  get('/sign_in', {to: 'sessions#new'})
+
+  resources :users, only:[:new, :create, :index, :show]
+  resources :sessions, only:[:new, :create] do
+    delete :destroy, on: :collection
+  end
+
 end
