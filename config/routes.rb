ee8b400ed53223d8ce2ca4d2c892804ff2d67f28
Derick Del Rosario <derickmitchel@icloud.com>
2019-10-26 04:56:43 -0700

    .

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
 
   resources :users, only:[:new, :create, :index, :show]
 
-
   resources :sessions, only: [:new, :create] do
     delete :destroy, on: :collection
   end
